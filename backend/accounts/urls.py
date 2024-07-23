d6e11df2ea1e054ad486c574e757f6f5bc01fb13
KingAde <theking-ade@outlook.com>
2024-07-23 23:22:03 +0100

    [add] url paths for verify mnemonic and reset password

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,11 +1,11 @@
-# accounts/urls.py
-
 from django.urls import path
-from .views import RegisterView, LoginView, SendTransactionView, TransactionListView
+from .views import RegisterView, LoginView, SendTransactionView, TransactionListView, VerifyMnemonicView, ResetPasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('send/', SendTransactionView.as_view(), name='send-transaction'),
     path('transactions/', TransactionListView.as_view(), name='transaction-history'),
+    path('verify-mnemonic/', VerifyMnemonicView.as_view(), name='verify-mnemonic'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),  # Add this line
 ]
